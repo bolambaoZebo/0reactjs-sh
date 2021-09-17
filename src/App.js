bab2c60e0ed617e4454d276c6d0d885f98243a19
bolamboaz <bolamboaz@gmail.com>
2021-09-17 10:47:38 +0800

    update ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
+
+  async function fetchData() {
+    try {
+      const result = await axios.get("https://www.scorebat.com/video-api/v3/");
+      console.log(result.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+    }, []);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
     </div>
   );
 }
