845629cfcaf5980c39895663ebd711fb10cd69fd
bolamboaz <bolamboaz@gmail.com>
2021-09-17 11:03:07 +0800

    update result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import axios from "axios";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
+
+  // const [ competition, title, videos] = useState()
 
   async function fetchData() {
     try {
       const result = await axios.get("https://www.scorebat.com/video-api/v3/");
-      console.log(result.data)
+      console.log(result.data.videos)
     } catch (error) {
       console.error(error);
     }
